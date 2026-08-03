fa78fd2f2404e83e4ae2465ee41a825272f7384c
Caio Motta Barcelos <caiomottabarcelos13@gmail.com>
2026-08-03 11:41:58 -0300

    :wrench:chore: gitignore e normalização de nomes

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from pathlib import Path
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 
 
 BASE_DIR = Path(__file__).resolve().parent
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory=BASE_DIR / "static"), name = "static")
 
 templates = Jinja2Templates(directory=BASE_DIR / "templates")
 
